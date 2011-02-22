05ccfedf4c7a2a19cf946e7108533a187623eb5b
roger.castaldo@gmail.com <roger.castaldo@gmail.com@285612a5-8603-5e9c-2405-8bdaedf257bd>
2011-02-22 14:54:05 +0000

    shifted the header concept to independent class for simple access. added in uploaded file object that is not setup to process yet. Added in remaining code to process parameters, I believe.

---
+++ Library/Components/HeaderCollection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Org.Reddragonit.EmbeddedWebServer.Components
+{
+    public class HeaderCollection
+    {
+        private Dictionary<string, string> _values;
+        public string this[string name]
+        {
+            get
+            {
+                if (_values.ContainsKey(name))
+                    return _values[name];
+                return null;
+            }
+            set
+            {
+                if (_values.ContainsKey(name))
+                    _values.Remove(name);
+                _values.Add(name, value);
+            }
+        }
+
+        public string Host
+        {
+            get { return this["Host"]; }
+        }
+
+        public string ContentLength
+        {
+            get { return this["Content-Length"]; }
+        }
+
+        public string ContentType
+        {
+            get { return this["Content-Type"]; }
+        }
+
+        public HeaderCollection()
+        {
+            _values = new Dictionary<string, string>();
+        }
+    }
+}
