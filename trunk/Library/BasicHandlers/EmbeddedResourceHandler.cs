42ab5f8adb7d243218da000e8712f4816c1600e2
roger.castaldo@gmail.com <roger.castaldo@gmail.com@285612a5-8603-5e9c-2405-8bdaedf257bd>
2011-02-22 19:14:25 +0000

    began to add in the minifiers and am embedded resource handler, need to implement proper stream screation including headers on the http connection socket in order to write the wesponse.

---
+++ trunk/Library/BasicHandlers/EmbeddedResourceHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Org.Reddragonit.EmbeddedWebServer.Interfaces;
+using Org.Reddragonit.EmbeddedWebServer.Components;
+
+namespace Org.Reddragonit.EmbeddedWebServer.BasicHandlers
+{
+    public class EmbeddedResourceHandler : IRequestHandler
+    {
+        #region IRequestHandler Members
+
+        bool IRequestHandler.IsReusable
+        {
+            get { return true; }
+        }
+
+        bool IRequestHandler.CanProcessRequest(HttpConnection conn, Site site)
+        {
+            if (site.EmbeddedFiles != null)
+            {
+                foreach (sEmbeddedFile file in site.EmbeddedFiles)
+                {
+                    if (file.URL == conn.URL.AbsolutePath)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        void IRequestHandler.ProcessRequest(HttpConnection conn,Site site)
+        {
+            sEmbeddedFile? file = null;
+            foreach (sEmbeddedFile ef in site.EmbeddedFiles)
+            {
+                if (ef.URL == conn.URL.AbsolutePath)
+                {
+                    file = ef;
+                    break;
+                }
+            }
+            switch (file.Value.FileType)
+            {
+                case EmbeddedFileTypes.Compressed_Css:
+                    break;
+            }
+        }
+
+        void IRequestHandler.Init()
+        {
+        }
+
+        #endregion
+    }
+}
