2bcc839e601c33d55f5135f8acd9d80cf14f32ae
roger.castaldo@gmail.com <roger.castaldo@gmail.com@285612a5-8603-5e9c-2405-8bdaedf257bd>
2012-05-25 14:54:18 +0000

    added into testing software to default trace to console.

--- trunk/Tester/Program.cs
+++ trunk/Tester/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Org.Reddragonit.EmbeddedWebServer;
+using Org.Reddragonit.EmbeddedWebServer.Diagnostics;
 
 namespace Tester
 {
@@ -9,6 +10,8 @@ namespace Tester
     {
         static void Main(string[] args)
         {
+            Logger.OverrideDiagnosticsLevel(DiagnosticsLevels.TRACE);
+            Logger.OverrideOutputLevel(DiagnosticsOutputs.CONSOLE);
             ServerControl.Start();
             Console.WriteLine("Press enter to shut down...");
             Console.ReadLine();
