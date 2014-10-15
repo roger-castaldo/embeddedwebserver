3c474615673536f0c15b148010479c486a6b6667
roger.castaldo <roger.castaldo@285612a5-8603-5e9c-2405-8bdaedf257bd>
2014-10-15 18:33:31 +0000

    corrected bug wrt assembly scanning

--- Library/Utility.cs
+++ Library/Utility.cs
@@ -119,7 +119,8 @@ namespace Org.Reddragonit.EmbeddedWebServer
                 }
                 catch (Exception e)
                 {
-                    if (e.Message != "The invoked member is not supported in a dynamic assembly.")
+                    if (e.Message != "The invoked member is not supported in a dynamic assembly."
+                        && e.Message!="Unable to load one or more of the requested types. Retrieve the LoaderExceptions property for more information.")
                     {
                         throw e;
                     }
