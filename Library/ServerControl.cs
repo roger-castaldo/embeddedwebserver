9db0ec2af5ee9094eb9a525dc3b804b7d2b8646a
roger-castaldo <roger.castaldo@gmail.com>
2016-02-01 13:52:32 -0500

    updated code base to allow servercontrol to be loaded from a separate appdomain

--- Library/ServerControl.cs
+++ Library/ServerControl.cs
@@ -11,7 +11,7 @@ using Org.Reddragonit.EmbeddedWebServer.Attributes;
 
 namespace Org.Reddragonit.EmbeddedWebServer
 {
-    public class ServerControl : IBackgroundOperationContainer
+    public class ServerControl : MarshalByRefObject,IBackgroundOperationContainer
     {
         /*
          * This is the main exposed class.  It is used to control the web server itself by calling to
@@ -188,6 +188,11 @@ namespace Org.Reddragonit.EmbeddedWebServer
         public static void UnRegisterBackgroundOperationPostCall(ServerControl.delPostBackgroundCall call)
         {
             _backgroundRunner.UnregisterPostCall(call);
+        }
+
+        public override object InitializeLifetimeService()
+        {
+            return null;
         }
     }
 }
